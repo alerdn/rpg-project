f53df0e1c458aacd27238957d1190a5899478bff
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2022-06-04 17:42:43 -0400

    contrução dialogue editor

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,81 @@ namespace RPG.Dialogue
     public class Dialogue : ScriptableObject
     {
         [SerializeField]
-        DialogueNode[] nodes;
+        List<DialogueNode> nodes = new List<DialogueNode>();
+
+        Dictionary<string, DialogueNode> nodeLookup = new Dictionary<string, DialogueNode>();
+
+        // Anotação para rodar essa função apenas no modo edição da Unity
+#if UNITY_EDITOR
+        private void Awake()
+        {
+            if (nodes.Count == 0)
+            {
+                DialogueNode rootNode = new DialogueNode
+                {
+                    uniqueID = Guid.NewGuid().ToString()
+                };
+                nodes.Add(rootNode);
+            }
+
+            OnValidate();
+        }
+#endif
+
+        /* OnValidate é como o Awake, ele executa quando carrega o ScriptableObject, mas também quando ele é editado
+         * Temos que chamar essa função manualmente no Awake() porque ela só é executada no Inspector
+         * então o dicionário não seria criado quando fôssemos jogar o jogo
+         */
+        private void OnValidate()
+        {
+            nodeLookup.Clear();
+            foreach (DialogueNode node in GetAllNodes())
+            {
+                nodeLookup[node.uniqueID] = node;
+            }
+        }
+
+        public IEnumerable<DialogueNode> GetAllNodes()
+        {
+            return nodes;
+        }
+
+        public DialogueNode GetRootNode()
+        {
+            return nodes[0];
+        }
+
+        public IEnumerable<DialogueNode> GetAllChildren(DialogueNode parentNode)
+        {
+            foreach (string childID in parentNode.children)
+            {
+                if (nodeLookup.ContainsKey(childID))
+                    yield return nodeLookup[childID];
+            }
+        }
+
+        public void CreateNode(DialogueNode parent)
+        {
+            DialogueNode childNode = new DialogueNode { uniqueID = Guid.NewGuid().ToString() };
+            parent.children.Add(childNode.uniqueID);
+            nodes.Add(childNode);
+            OnValidate();
+        }
+
+        public void DeleteNode(DialogueNode nodeToDelete)
+        {
+            nodes.Remove(nodeToDelete);
+            OnValidate();
+            CleanDanglingChildren(nodeToDelete);
+        }
+
+        private void CleanDanglingChildren(DialogueNode nodeToDelete)
+        {
+            foreach (DialogueNode node in GetAllNodes())
+            {
+                node.children.Remove(nodeToDelete.uniqueID);
+            }
+        }
     }
 }
 
--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -6,10 +7,27 @@ using UnityEngine;
 
 namespace RPG.Dialogue.Editor
 {
-
     public class DialogueEditor : EditorWindow
     {
         Dialogue selectedDialogue = null;
+        Vector2 scrollPosition;
+
+        [NonSerialized]
+        GUIStyle nodeStyle;
+        [NonSerialized]
+        DialogueNode draggingNode = null;
+        [NonSerialized]
+        Vector2 draggingOffset;
+        [NonSerialized]
+        DialogueNode creatingNode = null;
+        [NonSerialized]
+        DialogueNode deletingNode = null;
+        [NonSerialized]
+        DialogueNode linkingParentNode = null;
+        [NonSerialized]
+        bool draggingCanvas = false;
+        [NonSerialized]
+        Vector2 draggingCanvasOffset;
 
         [MenuItem("Window/Dialogue Editor")]
         public static void ShowEditorWindow()
@@ -23,7 +41,6 @@ namespace RPG.Dialogue.Editor
             Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
             if (dialogue != null)
             {
-                // selectedDialogue = dialogue;
                 ShowEditorWindow();
                 return true;
             }
@@ -34,6 +51,11 @@ namespace RPG.Dialogue.Editor
         private void OnEnable()
         {
             Selection.selectionChanged += OnSelectionChanged;
+
+            nodeStyle = new GUIStyle();
+            nodeStyle.normal.background = EditorGUIUtility.Load("node0") as Texture2D;
+            nodeStyle.padding = new RectOffset(20, 20, 20, 20);
+            nodeStyle.border = new RectOffset(12, 12, 12, 12);
         }
 
         private void OnSelectionChanged()
@@ -54,9 +76,189 @@ namespace RPG.Dialogue.Editor
             }
             else
             {
-                EditorGUILayout.LabelField($"{selectedDialogue.name}");
+                ProcessEvents();
+
+                scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+                GUILayoutUtility.GetRect(4000f, 4000f);
+
+                foreach (DialogueNode node in selectedDialogue.GetAllNodes())
+                {
+                    DrawConnections(node);
+                }
+                foreach (DialogueNode node in selectedDialogue.GetAllNodes())
+                {
+                    DrawNode(node);
+                }
+
+                EditorGUILayout.EndScrollView();
+
+                // Alteração na lista de nodes depois que terminamos de usá-la
+                if (creatingNode != null)
+                {
+                    Undo.RecordObject(selectedDialogue, "Added Dialogue Node");
+                    selectedDialogue.CreateNode(creatingNode);
+                    EditorUtility.SetDirty(selectedDialogue);
+                    creatingNode = null;
+                }
+                if (deletingNode != null)
+                {
+                    Undo.RecordObject(selectedDialogue, "Deleted Dialogue Node");
+                    selectedDialogue.DeleteNode(deletingNode);
+                    EditorUtility.SetDirty(selectedDialogue);
+                    deletingNode = null;
+                }
+            }
+        }
+
+        private void ProcessEvents()
+        {
+            if (Event.current.type == EventType.MouseDown && draggingNode == null)
+            {
+                draggingNode = GetNodeAtPoint(Event.current.mousePosition + scrollPosition);
+                if (draggingNode != null)
+                {
+                    draggingOffset = draggingNode.rect.position - Event.current.mousePosition;
+                }
+                else
+                {
+                    draggingCanvas = true;
+                    draggingCanvasOffset = Event.current.mousePosition + scrollPosition;
+                }
+            }
+            else if (Event.current.type == EventType.MouseDrag && draggingNode != null)
+            {
+                // Precisamos registrar um ponto para podermos voltar atrás
+                Undo.RecordObject(selectedDialogue, "Move Dialogue Node");
+
+                draggingNode.rect.position = Event.current.mousePosition + draggingOffset;
+
+                // Avisamos ao GUI que ele foi atualizado e precisa ser repintado
+                GUI.changed = true;
+            }
+            else if (Event.current.type == EventType.MouseDrag && draggingCanvas)
+            {
+                scrollPosition = draggingCanvasOffset - Event.current.mousePosition;
+
+                GUI.changed = true;
+            }
+            else if (Event.current.type == EventType.MouseUp && draggingNode != null)
+            {
+                EditorUtility.SetDirty(selectedDialogue);
+                draggingNode = null;
+            }
+            else if (Event.current.type == EventType.MouseUp && draggingCanvas)
+            {
+                draggingCanvas = false;
+            }
+        }
+
+        private void DrawNode(DialogueNode node)
+        {
+            GUILayout.BeginArea(node.rect, nodeStyle);
+
+            // Unity começa a ouvir se haverá alterações na UI
+            EditorGUI.BeginChangeCheck();
+
+            // Desenhar componentes no editor
+            /* Como em ReactJS, nós controlamos o valor do componente, 
+             * então precisamos ler o valor de uma variável e salvar
+             * essa variável quando ela for alterada 
+             */
+            EditorGUILayout.LabelField("Text:", EditorStyles.whiteLabel);
+            string newText = EditorGUILayout.TextField(node.text);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(selectedDialogue, "Update Dialogue Text");
+
+                // Fazer atualizações das variáveis aqui
+                node.text = newText;
+
+                EditorUtility.SetDirty(selectedDialogue);
             }
 
+            GUILayout.BeginHorizontal();
+
+            // Botões - Ao mesmo tempo que criamos, eles retornam true ou false se foram clicados
+            if (GUILayout.Button("+"))
+            {
+                creatingNode = node;
+            }
+            if (linkingParentNode == null)
+            {
+                if (GUILayout.Button("link"))
+                {
+                    linkingParentNode = node;
+                }
+            }
+            else if (linkingParentNode == node)
+            {
+                if (GUILayout.Button("cancel"))
+                {
+                    linkingParentNode = null;
+                }
+            }
+            else if (linkingParentNode.children.Contains(node.uniqueID))
+            {
+                if (GUILayout.Button("unlink"))
+                {
+                    Undo.RecordObject(selectedDialogue, "Removed Dialogue Link");
+                    linkingParentNode.children.Remove(node.uniqueID);
+                    EditorUtility.SetDirty(selectedDialogue);
+                    linkingParentNode = null;
+                }
+            }
+            else
+            {
+                if (GUILayout.Button("child"))
+                {
+                    Undo.RecordObject(selectedDialogue, "Added Dialogue Link");
+                    linkingParentNode.children.Add(node.uniqueID);
+                    EditorUtility.SetDirty(selectedDialogue);
+                    linkingParentNode = null;
+                }
+            }
+            if (GUILayout.Button("x"))
+            {
+                deletingNode = node;
+            }
+
+            GUILayout.EndHorizontal();
+
+            GUILayout.EndArea();
+        }
+
+        private void DrawConnections(DialogueNode node)
+        {
+            Vector3 startPosition = new Vector2(node.rect.xMax, node.rect.center.y);
+            foreach (DialogueNode chilNode in selectedDialogue.GetAllChildren(node))
+            {
+                Vector3 endPosition = new Vector2(chilNode.rect.xMin, chilNode.rect.center.y);
+
+                // Curvas
+                Vector3 controlPointOffset = endPosition - startPosition;
+                controlPointOffset.y = 0;
+                controlPointOffset.x *= 0.8f;
+
+                Handles.DrawBezier(
+                    startPosition, endPosition,
+                    startPosition + controlPointOffset, endPosition - controlPointOffset,
+                    Color.white, null, 4f);
+            }
+        }
+
+        private DialogueNode GetNodeAtPoint(Vector2 point)
+        {
+            DialogueNode foundNode = null;
+            foreach (DialogueNode node in selectedDialogue.GetAllNodes())
+            {
+                if (node.rect.Contains(point))
+                {
+                    foundNode = node;
+                }
+            }
+
+            return foundNode;
         }
     }
 }
