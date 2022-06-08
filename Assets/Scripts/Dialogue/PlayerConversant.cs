3fcdad16cb28b585c35da35ccdec04abc020eb2f
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2022-06-08 14:51:47 -0400

    criando UI de escolhas

--- Assets/Scripts/Dialogue/PlayerConversant.cs
+++ Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,17 +7,39 @@ namespace RPG.Dialogue
 {
     public class PlayerConversant : MonoBehaviour
     {
-        [SerializeField]
-        Dialogue currentDialogue;
+        [SerializeField] Dialogue currentDialogue;
+        DialogueNode currentNode = null;
+
+        private void Awake()
+        {
+            currentNode = currentDialogue.GetRootNode();
+        }
 
         public string GetText()
         {
-            if (currentDialogue == null)
+            if (currentNode == null)
             {
                 return "";
             }
 
-            return currentDialogue.GetRootNode().GetText();
+            return currentNode.GetText();
+        }
+
+        public IEnumerable<string> GetChoices()
+        {
+            yield return "General Kenobi";
+            yield return "Hi!";
+        }
+
+        public void Next()
+        {
+            DialogueNode[] children = currentDialogue.GetAllChildren(currentNode).ToArray();
+            currentNode = children[Random.Range(0, children.Count())];
+        }
+
+        public bool HasNext()
+        {
+            return currentDialogue.GetAllChildren(currentNode).Count() > 0;
         }
     }
 }
--- Assets/Scripts/UI/DialogueUI.cs
+++ Assets/Scripts/UI/DialogueUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using RPG.Dialogue;
 using TMPro;
 
@@ -10,18 +11,40 @@ namespace RPG.UI
     {
         PlayerConversant playerConversant;
         [SerializeField] TextMeshProUGUI AIText;
+        [SerializeField] Button nextButton;
+        [SerializeField] Transform choiceRoot;
+        [SerializeField] GameObject choicePrefab;
 
-        // Start is called before the first frame update
         void Start()
         {
             playerConversant = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConversant>();
-            AIText.text = playerConversant.GetText();
+            nextButton.onClick.AddListener(Next);
+
+            UpdateUI();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Next()
         {
+            playerConversant.Next();
+            UpdateUI();
+        }
 
+        private void UpdateUI()
+        {
+            AIText.text = playerConversant.GetText();
+            nextButton.gameObject.SetActive(playerConversant.HasNext());
+            
+            // Limpando e desenhando botões
+            foreach (Transform item in choiceRoot)
+            {
+                Destroy(item.gameObject);
+            }
+
+            foreach(string choiceText in playerConversant.GetChoices())
+            {
+                GameObject button = Instantiate(choicePrefab, choiceRoot);
+                button.GetComponentInChildren<TMP_Text>().text = choiceText;
+            }
         }
     }
 }
